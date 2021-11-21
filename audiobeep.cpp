d5333a82b3c55a34536270e13b43cac1866af6d7
lmunozbohollo <lucia.munozbohollo@gmail.com>
2021-11-21 11:29:49 +0000

    Playing integer

--- audiobeep.cpp
+++ audiobeep.cpp
@@ -4,74 +4,54 @@
 #include <math.h>
 
 // constructor
-AudioBeep::AudioBeep(QObject *w,qreal beepDuration, qreal beepFreq) {
+AudioBeep::AudioBeep(QObject *w, float duration, float frequency, float volume) {
 	qparent = w;
-	duration = beepDuration;
-	frequency = beepFreq;
-	const quint32 n = static_cast<quint32>(duration * sampleRate);   // number of data samples
-	// --- transfer QVector data to QByteBuffer
+	
+	audioFormat.setSampleRate(sampleRate);
+	audioFormat.setChannelCount(1);
+	audioFormat.setSampleSize(16);
+	audioFormat.setCodec("audio/pcm");
+	audioFormat.setByteOrder((QAudioFormat::Endian)QSysInfo::ByteOrder);
+	audioFormat.setSampleType(QAudioFormat::SignedInt);
+		
+	QAudioDeviceInfo deviceInfo(QAudioDeviceInfo::defaultOutputDevice());
+	if(!deviceInfo.isFormatSupported(audioFormat)) {
+		throw "Raw audio format not supported by backend, cannot play audio.";
+	}
+
+	// number of data samples
+	const unsigned int n = (unsigned int)round(duration * sampleRate);
+
 	// resize byteBuffer to the total number of bytes that will be needed to accommodate
 	// all the n data samples that are of type float
-	byteBuffer.resize(sizeof(float) * n);
-	for (quint32 i = 0; i < n; i++)
-	{
-		qreal sinVal = (qreal)sin(2.0 * M_PI * frequency * i / sampleRate);  // create sine wave data samples, one at a time
-		
-		// break down one float into four bytes
-		float sample = (float)sinVal;  // save one data sample in a local variable, so I can break it down into four bytes
-		char *ptr = (char*)(&sample);  // assign a char* pointer to the address of this data sample
-		char byte00 = *ptr;         // 1st byte
-		char byte01 = *(ptr + 1);   // 2nd byte
-		char byte02 = *(ptr + 2);   // 3rd byte
-		char byte03 = *(ptr + 3);   // 4th byte
-			
-		// put byte data into QByteArray, one byte at a time
-		byteBuffer[4 * i] = byte00;       // put 1st byte into QByteArray
-		byteBuffer[4 * i + 1] = byte01;   // put 2nd byte into QByteArray
-		byteBuffer[4 * i + 2] = byte02;   // put 3rd byte into QByteArray
-		byteBuffer[4 * i + 3] = byte03;   // put 4th byte into QByteArray
+	byteBuffer.resize(sizeof(qint16) * n);
+
+	// create the sinewave
+	for (unsigned int i = 0; i < n; i++) {
+		// create sine wave data samples, one at a time
+		qint16 sinVal = (qint16)(sin(2.0 * M_PI * frequency * i / sampleRate)*32767*volume);
+		( (qint16*)byteBuffer.constData() )[i] = sinVal;
 	}	
 }
 
+// play the audio
 void AudioBeep::play() {
-	// Make a QBuffer with our QByteArray
-	QBuffer* input  = new QBuffer(&byteBuffer);
-	input->open(QIODevice::ReadOnly);   // set the QIODevice to read-only
-		
-	// create and setup a QAudioFormat object
-	QAudioFormat audioFormat;
-	audioFormat.setSampleRate(static_cast<int>(sampleRate));
-	audioFormat.setChannelCount(1);
-	// set the sample size in bits. We set it to 32 bis, because we set SampleType to float (one float has 4 bytes ==> 32 bits)
-	audioFormat.setSampleSize(32);
-	audioFormat.setCodec("audio/pcm");
-	audioFormat.setByteOrder(QAudioFormat::LittleEndian);
-	// use Float, to have a better resolution than SignedInt or UnSignedInt
-	audioFormat.setSampleType(QAudioFormat::Float);
-		
-	// create a QAudioDeviceInfo object, to make sure that our audioFormat is supported by the device
-	QAudioDeviceInfo deviceInfo(QAudioDeviceInfo::defaultOutputDevice());
-	if(!deviceInfo.isFormatSupported(audioFormat))
-	{
-		qWarning() << "Raw audio format not supported by backend, cannot play audio.";
-		return;
-	}
-
+	QBuffer* input  = new QBuffer(&byteBuffer, qparent);
+	input->open(QIODevice::ReadOnly);
 	QAudioOutput* audio = new QAudioOutput(audioFormat, qparent);
-		
+	// Create a callback as a lambda expression which releases "input" and "audio"
+	// after playing has finished, thus this allows async audio playing without blocking.
 	connect(audio, &QAudioOutput::stateChanged, [audio, input](QAudio::State newState)
 							    {
 								    // finished playing (i.e., no more data)
 								    if (newState == QAudio::IdleState)
 								    {
+									    // delete the classes and release the memory
 									    delete audio;
 									    delete input;
 								    }
 							    });
-
-
-	// start the audio (i.e., play sound from the QAudioOutput object that we just created)
+	// Start the audio (i.e., play sound from the QAudioOutput object) and return immediately to the caller.
 	audio->start(input);
-
 }
 
--- audiobeep.h
+++ audiobeep.h
@@ -12,19 +12,19 @@ class AudioBeep : public QObject {
 	Q_OBJECT
 
 public:
-	AudioBeep(QObject *w, qreal beepDuration = 1.0, qreal beepFreq = 1000);
+	AudioBeep(QObject *w,	// w is the parent widget
+		  float beepDuration = 0.2,	// duration in seconds
+		  float beepFreq = 1000,	// beep frequency in Hz
+		  float volume = 1.0);		// volume of the tone, from 0 to 1
 
-public slots:
-	void play();
+	void play();	// plays sine wave asynchronously
 
 private:
+	const unsigned int sampleRate = 48000;	// sample rate
+
 	QObject* qparent;
-
-	qreal sampleRate = 48000;   // sample rate
-	qreal duration = 1.000;     // duration in seconds
-	qreal frequency = 1000;     // frequency
-
 	QByteArray byteBuffer;
+	QAudioFormat audioFormat;
 };
 
 #endif
